0074a4ccfd270ccd54b8a02049b173ab702442de
AllInOne & Rudra Sahoo <rntmixing@gmail.com>
2024-12-14 14:25:40 +0530

    new file

--- auth/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-if(isset($_COOKIE['username'])){
-    unset($_COOKIE['username']);
-    setcookie('username','', time()-3600,'/');
-    header(("Location: index.php"));
-    exit;
-};
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(isset($_COOKIE['username'])){
-    header('Location: dash.php');
-}else{
-    header('Location: auth/index.php');
-}
